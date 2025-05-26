c2a82155986c3f43c151963ba358446041f9438e
jungmin Han <jmni.hhh@gmail.com>
2025-05-26 23:04:18 +0900

    작은 오류 수정

--- 과제2-2-2/boundary/CloseUI.h
+++ 과제2-2-2/boundary/CloseUI.h
@@ -10,7 +10,7 @@ private:
     Close* closeControl;    // 클로즈 클래스 연결
 
 public:
-    CloseUI(Close* close);  //컨트롤클래스 가리키는 포인터
+    CloseUI(Close* control);  //컨트롤클래스 가리키는 포인터
 
     // 입력 처리하고 메세지 출력
     void input(ofstream& fout);
